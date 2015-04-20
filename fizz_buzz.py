7fbc1e1fa9afd2ab47bab9af02adf757b943f661
Alex Lyman <alexjohnlyman@gmail.com>
2015-04-20 08:13:26 -0600

    Fizz Buzz problem

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,6 +1,20 @@
 
+def fizz_buzz(file_name):
+    with open(file_name, 'r+') as f:
+        # line = []
+        lines = f.read()
+        str(lines)
+        # for i in lines:
+        #     line.append(i)
+        print lines[2]
 
 
+        # X = lines[0]
+        # Y = lines[1]
+        # N = lines[2]
+        # return lines
+
+fizz_buzz('test.txt')
 
 
 
