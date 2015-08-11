d27d5e4e47baac2c3836f4424a70d78d31dcbdbc
Alex Lyman <alexjohnlyman@gmail.com>
2015-08-11 12:08:20 -0600

    Completed odd numbers problem

---
+++ easy/odd_numbers.py
@@ -0,0 +1,11 @@
+# Print odd number from 1 - 99
+
+
+# def odd_numbers():
+#     for i in range(1, 100):
+#         if i % 2 == 0:
+#             continue
+#         else:
+#             print i
+#
+# odd_numbers()
